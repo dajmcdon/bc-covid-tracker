81be1294da1dfa119972240f3c3e38b7c37e7337
shizhemeng <mshizhe@students.cs.ubc.ca>
2021-11-20 18:16:41 -0800

    resolved requested changes, slighty changed formats

--- ploting.R
+++ ploting.R
@@ -1,53 +1,57 @@
-plotcovid <- function (data, 
-                       scale = c("s", "ls", "ps"), 
-                       by = c("all", "ha","age"), 
+plotcovid <- function (data,
+                       scale_y = c("none", "log_scale", "per_capita"),
+                       subgroups = c("all", "health_authority", "age"),
                        time = c("all", "recent")) {
-  scale <- match.arg(scale)
-  by <- match.arg(by)
-  if(scale == "ps") {
-    if(by == "all") data <- data %>% mutate(Cases = Cases / pop_bc * 1e5)
-    if(by == "ha" || by == "age") {
+  scale_y <- match.arg(scale_y)
+  subgroups <- match.arg(subgroups)
+  
+  scale_y <- switch (
+    scale_y,
+    none = "s",
+    log_scale = "ls",
+    per_capita = "ps"
+  )
+  if (scale_y == "ps") {
+    if (subgroups == "all")
+      data <- data %>% mutate(Cases = Cases / pop_bc * 1e5)
+    if (subgroups == "health_authority" || subgroups == "age") 
       data <- data %>% mutate(Cases = Cases / pop * 1e5)
-      }
   }
-
-  # TODO: Could use a if statement if desired
-  # TODO: could potentially pull out some more code using do.call if desired
-  p <- switch(by,
-              all = ggplot(data, aes(Date, Cases))+
-                geom_point(colour = "darkblue", shape = 16, alpha = .4) +
-                geom_line(aes_string(y = scale), size = 1.5, colour = "darkblue"),
-              
-              ha = ggplot(data, aes(Date, Cases, colour = HA)) +
-                geom_point(shape = 16, alpha = .4) +
-                geom_line(aes_string(y = scale), size = 1.5) +
-                scale_colour_brewer(palette = "Set1") +
-                theme(legend.title = element_blank(), legend.position = "bottom"),
-              
-              age = ggplot(data, aes(Date, Cases, colour = Age)) +
-                geom_point(shape = 16, alpha = .4) +
-                geom_line(aes_string(y = scale), size = 1.5) +
-                scale_colour_viridis_d() +
-                theme(legend.title = element_blank(),
-                      legend.position = "bottom")
+  
+  p <- switch (subgroups,
+    all = ggplot(data, aes(Date, Cases)) +
+      geom_point(colour = "darkblue", shape = 16,alpha = .4) +
+      geom_line(aes_string(y = scale_y), size = 1.5, colour = "darkblue"),
+    
+    health_authority = ggplot(data, aes(Date, Cases, colour = HA)) +
+      geom_point(shape = 16, alpha = .4) +
+      geom_line(aes_string(y = scale_y), size = 1.5) +
+      scale_colour_brewer(palette = "Set1") +
+      theme(legend.title = element_blank(), legend.position = "bottom"),
+    
+    age = ggplot(data, aes(Date, Cases, colour = Age)) +
+      geom_point(shape = 16, alpha = .4) +
+      geom_line(aes_string(y = scale_y), size = 1.5) +
+      scale_colour_viridis_d() +
+      theme(legend.title = element_blank(),
+            legend.position = "bottom")
   )
   
-
-  # For different ylab and y scales
-  p <-  switch (scale,
-                s = p,
-                ls = p + 
-                  scale_y_continuous(trans = pseudo_log_trans(base = 10), 
-                                     breaks = log10_breaks) +
-                  ylab("Cases (log scale)"),
-                ps = p +
-                  ylab("Cases per 100K population")
+  # Add y aesthetics based on `scale_y`
+  p <-  switch (scale_y,
+    s = p,
+    ls = p +
+      scale_y_continuous(trans = pseudo_log_trans(base = 10),
+                         breaks = log10_breaks) +
+      ylab("Cases (log scale_y)"),
+    ps = p +
+      ylab("Cases per 100K population")
   )
   
-  # scale x based on time
-  p <-  switch(time,
-               all = p + scale_x_date(date_breaks = "3 months", date_labels = "%b %Y"),
-               recent = p
+  # Add x aesthetics based on `time`
+  p <-  switch (time,
+    all = p + scale_x_date(date_breaks = "3 months", date_labels = "%b %Y"),
+    recent = p
   )
   p 
 }
